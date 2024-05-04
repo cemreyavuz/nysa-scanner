1f16de9cd1f1e4a8091b662fa8232696d4e44ae8
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2024-05-04 12:19:50 +0200

    feat: initialize scanner code

---
+++ src/scanner.ts
@@ -0,0 +1,97 @@
+import scanner, { ReactScannerConfig } from "react-scanner";
+import path from "path";
+import fs from "fs";
+
+export const scan = async (config: ReactScannerConfig) => {
+  return await scanner.run(config);
+};
+
+type RunProps = {
+  projectRoot: string;
+  srcDirPath?: string;
+  pkgJsonPath?: string;
+};
+
+const run = async ({ projectRoot, srcDirPath, pkgJsonPath }: RunProps) => {
+  const packageJson = JSON.parse(
+    fs.readFileSync(
+      path.resolve(pkgJsonPath ?? path.resolve(projectRoot, "package.json")),
+      { encoding: "utf-8" }
+    )
+  ) as { dependencies: Record<string, string> };
+  const dependencies = Object.keys(packageJson.dependencies);
+
+  const scanResults = await scan({
+    crawlFrom: path.resolve(srcDirPath ?? path.resolve(projectRoot, "src")),
+    processors: ["raw-report"],
+    rootDir: path.resolve(projectRoot),
+  });
+
+  const formatted: Record<
+    string,
+    {
+      bindings: Record<
+        string,
+        {
+          instanceCount: number;
+          props: Record<string, number>;
+        }
+      >;
+    }
+  > = {};
+
+  Object.entries(scanResults).forEach(([componentName, { instances }]) => {
+    instances.forEach(({ importInfo, props }) => {
+      // pass if importInfo doesn't exist
+      if (!importInfo) {
+        return;
+      }
+      const { moduleName } = importInfo;
+
+      // pass if module is not a dependency
+      if (!dependencies.includes(moduleName)) {
+        return;
+      }
+
+      // create library entry if it doesn't exist
+      if (!formatted[moduleName]) {
+        formatted[moduleName] = { bindings: {} };
+      }
+
+      // create binding entry if it doesn't exist
+      if (!formatted[moduleName].bindings[componentName]) {
+        formatted[moduleName].bindings[componentName] = {
+          instanceCount: 0,
+          props: {},
+        };
+      }
+
+      // increment instance count
+      formatted[moduleName].bindings[componentName].instanceCount += 1;
+
+      // add prop entries if they don't exist
+      Object.keys(props).forEach((propName) => {
+        if (!formatted[moduleName].bindings[componentName].props[propName]) {
+          formatted[moduleName].bindings[componentName].props[propName] = 0;
+        }
+      });
+
+      // increment prop counts
+      Object.keys(props).forEach((propName) => {
+        formatted[moduleName].bindings[componentName].props[propName] += 1;
+      });
+    });
+  });
+
+  const combined = Object.entries(formatted).map(
+    ([libraryName, { bindings }]) => ({
+      library: libraryName,
+      bindings: Object.entries(bindings).map(([componentName, info]) => ({
+        name: componentName,
+        ...info,
+      })),
+    })
+  );
+
+  return combined;
+};
